c6e7c5c664eee730233987294474f02952c825e6
shubham0419 <shubhamsc704707@gmail.com>
2025-08-18 10:47:38 +0530

    cd

--- lecture20_Auth/routes/auth.route.js
+++ lecture20_Auth/routes/auth.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { pluralize } = require("mongoose");
 
 router.post("/signup",async(req,res)=>{
   try {
@@ -36,9 +38,24 @@ router.post("/login",async(req,res)=>{
     if(!isMatched){
       throw new Error("Invalid email or password");
     }
-    res.status(200).json({message:"You are loggedin"})
+    const token = jwt.sign({id:user._id,name:user.name},process.env.JWT_SECRET,
+      {expiresIn:'1h',algorithm:'HS256'}
+    );
+    res.status(200).json({message:"You are loggedin",token:token})
   } catch (error) {
     res.status(400).json({message:error.message});
+  }
+})
+
+router.get("/check",async (req,res)=>{
+  try {
+    const authorization = req.headers.authorization;
+    const token  = authorization.split(" ")[1];
+    const payload = jwt.verify(token,process.env.JWT_SECRET);
+    // res.send("hello");
+    res.status(200).json({user:payload});
+  } catch (error) {
+    res.status(400).json({message:error.message})
   }
 })
 
