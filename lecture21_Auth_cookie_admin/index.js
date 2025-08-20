4b312c86150d0a5f064461d6e7886d52a415006b
shubham0419 <shubhamsc704707@gmail.com>
2025-08-20 11:00:22 +0530

    login auth & cookie

---
+++ lecture21_Auth_cookie_admin/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./db/connectDb");
+const app = express();
+const PORT = 4000;
+require("dotenv").config();
+// routers
+const authRouter = require("./routes/auth.route")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/auth",authRouter);
+app.get("/", (req, res) => {	});
+
+connectDB().then(()=>{
+  app.listen(PORT, () => console.log("Server running on port " + PORT));
+}).catch((error)=>console.log(error));
+
