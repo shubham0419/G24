84c8665132246c3b217a9ec2a9ca67478945af26
shubham0419 <shubhamsc704707@gmail.com>
2025-08-25 10:52:20 +0530

    premium auth

---
+++ lecture23_Auth_premium/routes/product.route.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const Product = require("../models/product.model");
+const User = require("../models/user.model");
+const router = express.Router();
+
+router.get("/create",async(req,res)=>{
+  const products = [
+    {
+      name:"product 1",
+      price:500,
+      description:"this is product 1"
+    },
+    {
+      name:"product 2",
+      price:500,
+      description:"this is product 2"
+    },
+    {
+      name:"product 3",
+      price:500,
+      description:"this is product 3"
+    },
+    {
+      name:"product 4",
+      price:500,
+      description:"this is product 4"
+    }
+  ]
+  try {
+    const products = await Product.insertMany(products);
+    res.status(200).json({products});
+  } catch (error) {
+    res.status(400).json({message:error.message})
+  }
+})
+
+router.get("/gold/discount/:productId",async(req,res)=>{
+  try {
+    const {productId} = req.params;
+    const product = await Product.findById(productId);
+    const user = await User.findById(req.user.id);
+    // gold discount -> 10%
+    if(user.credits - (product.price - 0.1*product.price) <0){
+      throw new Error("You don't have enough credits")
+    }
+    const discountedPrice = product.price - 0.1*product.price;
+    res.status(200).json({discountedPrice})
+  } catch (error) {
+    res.status(400).json({message:error.message});
+  }
+})
+
+module.exports = router;
