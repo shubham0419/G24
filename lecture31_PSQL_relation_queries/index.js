b1147094f93d85fe16aefa46c72e0d8ad89dd026
shubham0419 <shubhamsc704707@gmail.com>
2025-10-06 10:47:07 +0530

    psql queries

---
+++ lecture31_PSQL_relation_queries/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRouter = require("./routes/user.routes")
+const postRouter = require("./routes/post.routes")
+
+app.use("/user",userRouter);
+app.use("/post",postRouter);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
